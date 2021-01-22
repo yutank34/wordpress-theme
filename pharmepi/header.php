b2f18bec6f4f462f5b8603412f9228b8acaa3692
yutank34 <yuta34@gmail.com>
2021-01-22 22:22:46 +0900

    feat: update header

--- pharmepi/header.php
+++ pharmepi/header.php
@@ -46,7 +46,7 @@
 
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-				<div id="inner-header" class="wrap cf">
+				<div id="inner-header" class="wrap cf header">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
 					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
