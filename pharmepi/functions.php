4e8f655030dcf41946f5f47d8091075ccbc7964e
yutank34 <yuta34@gmail.com>
2021-01-23 18:27:53 +0900

    feat: add cutom type

--- pharmepi/functions.php
+++ pharmepi/functions.php
@@ -244,4 +244,23 @@ function bones_fonts() {
 
 add_action('wp_enqueue_scripts', 'bones_fonts');
 
+
+function create_post_type() {
+  $exampleSupports = [  // supports のパラメータを設定する配列（初期値だと title と editor のみ投稿画面で使える）
+    'title',  // 記事タイトル
+    'editor',  // 記事本文
+    'thumbnail',  // アイキャッチ画像
+    'revisions'  // リビジョン
+  ];
+  register_post_type( 'research-details',  // カスタム投稿ID
+    array(
+      'label' => '研究詳細',  // カスタム投稿名(管理画面の左メニューに表示されるテキスト)
+      'public' => true,  // 投稿タイプをパブリックにするか否か
+      'has_archive' => true,  // アーカイブ(一覧表示)を有効にするか否か
+      'menu_position' => 5,  // 管理画面上でどこに配置するか今回の場合は「投稿」の下に配置
+      'supports' => $exampleSupports  // 投稿画面でどのmoduleを使うか的な設定
+    )
+  );
+}
+add_action( 'init', 'create_post_type' ); // アクションに上記関数をフックします
 /* DON'T DELETE THIS CLOSING TAG */ ?>
