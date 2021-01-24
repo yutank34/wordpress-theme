b3544501b39268810ca9c218c2406ce177b3596a
yutank34 <yuta34@gmail.com>
2021-01-25 01:47:56 +0900

    feat: display blogs by tags

--- pharmepi/page-reserch_detail.php
+++ pharmepi/page-reserch_detail.php
@@ -1,9 +1,9 @@
-<?php add_filter( 'wp_head', function() {
-    ?>
-    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/html/tab/tab.css">
+<?php add_filter('wp_head', function () {
+?>
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/html/tab/tab.css">
 	<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/html/tab/tab.js"></script>
-    <?php
-} );
+<?php
+});
 ?>
 
 <?php get_header(); ?>
@@ -54,7 +54,31 @@
 						echo '</ul>';
 					}
 					?>
-					<?php comments_template(); ?>
+
+					<h2>get_posts</h2>
+					<?php
+					$hoge = 'hoge';
+					?>
+					<?php
+					$fuga = 'fuga';
+					echo $hoge;
+					echo $fuga;
+					?>
+					<?php
+					$args = array('posts_per_page' => -1, 'order' => 'ASC', 'orderby' => 'title', 'post_type' => 'research-details');
+					$postslist = get_posts($args);
+					foreach ($postslist as $post) :
+						setup_postdata($post); ?>
+						<div>
+							<?php the_date(); ?>
+							<br />
+							<?php the_title(); ?>
+							<?php the_excerpt(); ?>
+						</div>
+					<?php
+					endforeach;
+					wp_reset_postdata();
+					?>
 			<?php endwhile;
 	endif; ?>
 				</section>
@@ -62,16 +86,65 @@
 					<div class="tab-panel">
 						<!--タブ-->
 						<ul class="tab-group">
-							<li class="tab tab-A is-active">Tab-A</li>
+							<?php
+							$terms = get_terms('details');
+							if (!empty($terms) && !is_wp_error($terms)) {
+								$isFirst = true;
+								foreach ($terms as $term) {
+									$classParam = 'tab tab-' . $term->term_id;
+									if ($isFirst) {
+										$classParam = $classParam . ' is-active';
+									}
+									echo '<li class="' . $classParam . '">' . $term->name . '</li>';
+									$isFirst = false;
+								}
+							}
+							?>
+							<!-- <li class="tab tab-A is-active">Tab-A</li>
 							<li class="tab tab-B">Tab-B</li>
-							<li class="tab tab-C">Tab-C</li>
+							<li class="tab tab-C">Tab-C</li> -->
 						</ul>
 
 						<!--タブを切り替えて表示するコンテンツ-->
 						<div class="panel-group">
-							<div class="panel tab-A is-show">Content-A</div>
+							<?php
+							$isFirst = true;
+							foreach ($terms as $term) :
+								$tax_posts = get_posts( array(
+									'post_type' => 'research-details',
+									'posts_per_page' => -1, // 表示させたい記事数
+									'tax_query' => array(
+									  array(
+										'taxonomy' => 'details',
+										'terms' => array( $term->slug ),
+										'field' => 'slug',
+										'include_children' => true, //子タクソノミーを含める
+									  )
+									)
+								));
+								if( $tax_posts ) :	
+									$panelArgs = 'panel tab-'.$term->term_id;
+									if ($isFirst) {
+										$panelArgs = $panelArgs.' is-show';
+									}
+									$isFirst = false
+								?>
+								<div class="<?php echo $panelArgs; ?>">
+									<ul>
+										<?php foreach($tax_posts as $tax_post): ?>
+										<li>
+											<a href="<?php echo get_permalink($tax_post->ID); ?>"><?php echo get_the_title($tax_post->ID); ?></a>
+										</li>
+										<?php endforeach; wp_reset_postdata(); ?>
+									</ul>
+								</div>
+								<?php
+								endif;
+								endforeach;
+								?>
+							<!-- <div class="panel tab-A is-show">Content-A</div>
 							<div class="panel tab-B">Content-B</div>
-							<div class="panel tab-C">Content-C</div>
+							<div class="panel tab-C">Content-C</div> -->
 						</div>
 					</div>
 				</section>
