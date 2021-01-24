5f8a794c457c1cddb47dbe468d6a6f59ab3a9197
yutank34 <yuta34@gmail.com>
2021-01-24 21:05:52 +0900

    feat: update template

--- pharmepi/functions.php
+++ pharmepi/functions.php
@@ -252,15 +252,39 @@ function create_post_type() {
     'thumbnail',  // アイキャッチ画像
     'revisions'  // リビジョン
   ];
+  $exampleTags = array('details');
+    
+    // '学会発表',
+    // 'その他講演',
+    // '出版物'
+  // ];
   register_post_type( 'research-details',  // カスタム投稿ID
     array(
       'label' => '研究詳細',  // カスタム投稿名(管理画面の左メニューに表示されるテキスト)
       'public' => true,  // 投稿タイプをパブリックにするか否か
       'has_archive' => true,  // アーカイブ(一覧表示)を有効にするか否か
       'menu_position' => 5,  // 管理画面上でどこに配置するか今回の場合は「投稿」の下に配置
-      'supports' => $exampleSupports  // 投稿画面でどのmoduleを使うか的な設定
+      'supports' => $exampleSupports,  // 投稿画面でどのmoduleを使うか的な設定
+      'taxonomies' => $exampleTags
     )
   );
 }
 add_action( 'init', 'create_post_type' ); // アクションに上記関数をフックします
+
+function register_blog_cat_custom_post() {
+    register_taxonomy(
+        'details',
+        'research-details',
+        array(
+            'hierarchical' => true,
+            'label' => 'ブログのカテゴリ',
+            'show_ui' => true,
+            'query_var' => true,
+            'rewrite' => true,
+            'singular_label' => 'ブログのカテゴリ'
+        )
+    );
+}
+add_action('init', 'register_blog_cat_custom_post');
+
 /* DON'T DELETE THIS CLOSING TAG */ ?>
