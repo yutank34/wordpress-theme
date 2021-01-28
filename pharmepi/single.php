b4e45a1c6dc6a602cd317811159bb08a81d16a5a
yutank34 <yuta34@gmail.com>
2021-01-28 21:46:47 +0900

    feat: modify body color

--- pharmepi/single.php
+++ pharmepi/single.php
@@ -4,7 +4,7 @@
 
 				<div id="inner-content" class="wrap cf">
 
-					<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+					<main id="main" class="m-all t-2of3 d-5of7 wrap cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
 						<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
@@ -44,8 +44,6 @@
 						<?php endif; ?>
 
 					</main>
-
-					<?php get_sidebar(); ?>
 
 				</div>
 
