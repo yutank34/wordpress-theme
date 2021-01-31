e77008ca863c936f31d09e2a0b843701aef88d21
yutank34 <yuta34@gmail.com>
2021-01-31 20:39:58 +0900

    feat: update page format

--- pharmepi/post-formats/format.php
+++ pharmepi/post-formats/format.php
@@ -20,16 +20,6 @@
 
                   <h1 class="entry-title single-title" itemprop="headline" rel="bookmark"><?php the_title(); ?></h1>
 
-                  <p class="byline entry-meta vcard">
-
-                    <?php printf( __( 'Posted', 'bonestheme' ).' %1$s %2$s',
-                       /* the time the post was published */
-                       '<time class="updated entry-time" datetime="' . get_the_time('Y-m-d') . '" itemprop="datePublished">' . get_the_time(get_option('date_format')) . '</time>',
-                       /* the author of the post */
-                       '<span class="by">'.__( 'by', 'bonestheme' ).'</span> <span class="entry-author author" itemprop="author" itemscope itemptype="http://schema.org/Person">' . get_the_author_link( get_the_author_meta( 'ID' ) ) . '</span>'
-                    ); ?>
-
-                  </p>
 
                 </header> <?php // end article header ?>
 
@@ -58,14 +48,6 @@
                     ) );
                   ?>
                 </section> <?php // end article section ?>
-
-                <footer class="article-footer">
-
-                  <?php printf( __( 'filed under', 'bonestheme' ).': %1$s', get_the_category_list(', ') ); ?>
-
-                  <?php the_tags( '<p class="tags"><span class="tags-title">' . __( 'Tags:', 'bonestheme' ) . '</span> ', ', ', '</p>' ); ?>
-
-                </footer> <?php // end article footer ?>
 
                 <?php //comments_template(); ?>
 
