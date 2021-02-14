ea737b2f669e6a3df27ae9363ac1b0efafab74d5
yutank34 <yuta34@gmail.com>
2021-02-14 10:07:20 +0900

    feat: update header title

--- pharmepi/header.php
+++ pharmepi/header.php
@@ -49,7 +49,7 @@
 				<div id="inner-header" class="wrap cf header">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<h1 id="logo" class="homepage-name" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow">明治薬科大学</br><?php bloginfo('name'); ?></a></h1>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
