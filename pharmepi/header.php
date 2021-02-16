7f0c5befe331830b1695a1355f347b629d520182
yutank34 <yuta34@gmail.com>
2021-02-17 01:35:57 +0900

    feat: fix header

--- pharmepi/header.php
+++ pharmepi/header.php
@@ -19,14 +19,14 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/assets/lab_logo_t1_bk.png">
+		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/assets/lab_logo_t1_bk.png">
 		<!--[if IE]>
 			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 		<![endif]-->
 		<?php // or, set /favicon.ico for IE10 win ?>
 		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/lab_logo_t1_bk.png">
             <meta name="theme-color" content="#121212">
 
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
@@ -47,9 +47,11 @@
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
 				<div id="inner-header" class="wrap cf header">
-
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-					<h1 id="logo" class="homepage-name" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow">明治薬科大学</br><?php bloginfo('name'); ?></a></h1>
+					<a href="<?php echo home_url(); ?>" rel="nofollow" class="header-home-link">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/lab_logo_t1_bk.png" class="header-image">
+						<h1 id="logo" class="homepage-name" itemscope itemtype="http://schema.org/Organization">明治薬科大学</br><?php bloginfo('name'); ?></h1>
+					</a>
 
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
