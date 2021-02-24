34f9cb7446343eb587a8c65a6abcadff0d83c504
yutank34 <yuta34@gmail.com>
2021-02-24 23:03:18 +0900

    feat: fix index.html

--- pharmepi/index.php
+++ pharmepi/index.php
@@ -1,10 +1,3 @@
-<?php add_filter('wp_head', function () {
-?>
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/html/top/style.css">
-<?php
-});
-?>
-
 <?php get_header(); ?>
 <?php
 $tax_posts = get_posts(array(
@@ -25,86 +18,71 @@ if ($tax_posts) : ?>
 <?php endif; ?>
 <div id="content">
 	<div class="slider">
-		<figure>
-			<img src="<?php echo get_template_directory_uri(); ?>/assets/slide.jpg" alt="" width="1024" height="500">
-			<div>
-				<h1>公衆衛生・疫学研究室</h1>
+		<figure class="slider__figure">
+			<!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/slide.jpg" alt="" width="1024" height="500"> -->
+			<img src="<?php echo get_template_directory_uri(); ?>/assets/slide.jpg" alt="" class="slider__figure--image">
+			<div class="slider__figure--text-wrapper">
+				<h1 class="slider__figure--text">公衆衛生・疫学研究室</h1>
 			</div>
 		</figure>
 	</div>
 	<div id="inner-content" class="cf">
 		<main id="main" class="" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 			<div class="wrap box-container">
-				<h2>薬と患者をとりまく社会に関する研究・教育活動の実施</h2>
-				<ul>
-					<li class="box-description">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon_pc.png" class="box-container__icon">
-						<p class="box-title">薬剤疫学研究</p>
-						<p>医薬品の適正使用や副作用リスクを評価する研究</p>
+				<h2 class="box-container__header">薬と患者をとりまく社会に関する研究・教育活動の実施</h2>
+				<ul class="inner-items">
+					<li class="inner-items__item">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon_pc.png" class="inner-items__item--icon">
+						<p class="inner-items__item--title">薬剤疫学研究</p>
+						<p class="inner-items__item--discription">医薬品の適正使用や副作用リスクを評価する研究</p>
 					</li>
-					<li class="box-description">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon_economy.png" class="box-container__icon">
-						<p class="box-title">薬剤経済学研究</p>
-						<p>医療の費用対効果や患者QOLを取り扱う研究</p>
+					<li class="inner-items__item--wrapper">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon_economy.png" class="inner-items__item--icon">
+						<p class="inner-items__item--title">薬剤経済学研究</p>
+						<p class="inner-items__item--discription">医療の費用対効果や患者QOLを取り扱う研究</p>
 					</li>
-					<li class="box-description">
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon_health.png" class="box-container__icon">
-						<p class="box-title">ヘルスサービス研究</p>
-						<p>医療機関や地域で活躍する薬剤師の役割を考える研究</p>
+					<li class="inner-items__item--wrapper">
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/icon_health.png" class="inner-items__item--icon">
+						<p class="inner-items__item--title">ヘルスサービス研究</p>
+						<p class="inner-items__item--discription">医療機関や地域で活躍する薬剤師の役割を考える研究</p>
 					</li>
 				</ul>
 			</div>
-			<div class="back-color">
-				<div class="wrap box-container discription">
+			<div class="background-color--salmon box-container">
+				<div class="wrap  box-container__text--discription">
 					<p>公衆衛生・疫学研究室では、薬と患者をとりまく社会に関する研究を行っています。適切かつ効率的な薬物治療は薬と患者の関係だけでなく、それを取り巻く医療環境である社会全体を理解することが大切です。そのため、医薬品の適正使用や副作用リスクを評価する「薬剤疫学」、医療の費用対効果や患者QOLを取り扱う「薬剤経済学」、医療機関や地域で活躍する薬剤師の役割を考える「ヘルスサービス研究」を3つの柱として研究・教育活動を行ってます。薬物治療に関連した情報を「つくり」「つたえ」「つかう」ことは、薬剤師・薬学教育の中で重要になってきています。</p>
 				</div>
 			</div>
 			<div>
 				<div class="wrap box-container">
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/labo-description.jpg" class="box-container__img--large">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/labo-description.jpg" class="box-container__image--large">
 				</div>
-				<div class="back-color">
-					<div class="wrap box-container discription">
-						<ul>
-							<li>
-								<div class="sub-menu">
-									<a href="<?php echo home_url(); ?>/labo">
-										<img src="<?php echo get_template_directory_uri(); ?>/assets/about_labo.jpg" alt="">
-										<div>
-											<p>研究室の紹介</p>
-										</div>
-									</a>
-								</div>
+				<div class="background-color--salmon box-container">
+					<div class="wrap">
+						<ul class="inner-items">
+							<li class="inner-items__submenu">
+								<a href="<?php echo home_url(); ?>/labo">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/about_labo.jpg" alt="" class="inner-items__submenu--image">
+									<p class="inner-items__submenu--text">研究室の紹介</p>
+								</a>
 							</li>
-							<li>
-								<div class="sub-menu">
-									<a href="<?php echo home_url(); ?>/reserch">
-										<img src="<?php echo get_template_directory_uri(); ?>/assets/stock-photo-book-in-library-with-open-textbook-education-learning-concept-1274296330.jpg" alt="">
-										<div>
-											<p>研究内容の紹介</p>
-										</div>
-									</a>
-								</div>
+							<li class="inner-items__submenu">
+								<a href="<?php echo home_url(); ?>/reserch">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/stock-photo-book-in-library-with-open-textbook-education-learning-concept-1274296330.jpg" alt="" class="inner-items__submenu--image">
+									<p class="inner-items__submenu--text">研究内容の紹介</p>
+								</a>
 							</li>
-							<li>
-								<div class="sub-menu">
-									<a href="<?php echo home_url(); ?>/work">
-										<img src="<?php echo get_template_directory_uri(); ?>/assets/stock-photo-speaker-giving-a-talk-in-conference-hall-at-business-event-audience-at-the-conference-hall-1117902230.jpg" alt="">
-										<div>
-											<p>研究業績の紹介</p>
-										</div>
-									</a>
-								</div>
+							<li class="inner-items__submenu">
+								<a href="<?php echo home_url(); ?>/work">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/stock-photo-speaker-giving-a-talk-in-conference-hall-at-business-event-audience-at-the-conference-hall-1117902230.jpg" alt="" class="inner-items__submenu--image">
+									<p class="inner-items__submenu--text">研究業績の紹介</p>
+								</a>
 							</li>
-							<li>
-								<div class="sub-menu">
-									<a href="<?php echo home_url(); ?>/links">
-										<img src="<?php echo get_template_directory_uri(); ?>/assets/stock-photo-smart-city-and-iot-internet-of-things-concept-ict-information-communication-technology-1160793658.jpg" alt="">
-										<div>
-											<p>リンク</p>
-										</div>
-									</a>
-								</div>
+							<li class="inner-items__submenu">
+								<a href="<?php echo home_url(); ?>/links">
+									<img src="<?php echo get_template_directory_uri(); ?>/assets/stock-photo-smart-city-and-iot-internet-of-things-concept-ict-information-communication-technology-1160793658.jpg" alt="" class="inner-items__submenu--image">
+									<p class="inner-items__submenu--text">リンク</p>
+								</a>
 							</li>
 						</ul>
 					</div>
